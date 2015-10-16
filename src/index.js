d5c9434444ca9c36184b826608a38d30a0eb3a54
Ng Zhi An <ngzhian@gmail.com>
2015-10-16 08:54:55 +0000

    Initial project setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactART from 'react-art';
+import ReactDOM from 'react-dom';
+import Component from './Component';
+import VectorWidget from './VectorWidget';
+import Stage from './Stage';
+
+ReactDOM.render(<Stage />, document.getElementById('container'));

240b6b6812aea18e9f40f166861c1a6211225b3b
Oswell Chan <oswellchan@gmail.com>
2015-10-17 17:53:45 +0800

    Fix build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactART from 'react-art';
 import ReactDOM from 'react-dom';
 import Component from './Component';
-import VectorWidget from './VectorWidget';
 import Stage from './Stage';
 
 ReactDOM.render(<Stage />, document.getElementById('container'));
